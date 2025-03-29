6fa02ca891339db9c2fb6343c4f5ee078e50634a
Isaac Ojigho <v23565w@newrez.loan>
2025-03-29 12:49:28 -0500

    Just a quick Hello

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello everybody!");
+
+Console.WriteLine("Please enter your name: ");
+
+string name = Console.ReadLine(); // how to accept from the user who types
+
+Console.WriteLine("Hello " + name + " you are beautiful asf, i hope your studying going well, love and kisses babygirl ");
+
+// dotnet new will create a new project
+// dotnet build will compile my application
+// dotnet run will execte the app
